5b7e8daa9f0b5f691543de637371fb1a4b665345
Ariunzayaaaa <zzzaya98@gmail.com>
2023-10-04 12:12:17 +0900

    commit

---
+++ express_sample/server.js
@@ -0,0 +1,33 @@
+//express モジュール読み込み
+const express = require('express')
+//dotenvモジュール読み込み
+const dotenv = require('dotenv')
+
+//dotenvの設定読み込み
+dotenv.config()
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+console.log(HOST)
+console.log(PORT)
+
+//サーバー作成
+const app = express()
+
+//GETリクエストの処理
+app.get('/', (req, res) => {
+    //リクエストの処理
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    //レスポンスの処理
+    res.send('Hello!!!!!')
+})
+
+//サーバー待機（Listen）
+app.listen(PORT, HOST, () => {
+    console.log(HOST)
+    console.log(PORT)
+    console.log('wait...')
+})
